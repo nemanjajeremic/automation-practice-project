61ee7ad8fefde76fca31a6cb1c15ea7b8e31abd3
Nemanja Jeremic <n.jeremic@levi9.com>
2019-02-27 16:52:09 +0100

    blabla

--- tests/home/home.page.js
+++ tests/home/home.page.js
@@ -1,15 +1,12 @@
 let homePage = function () {
+
     this.searchInput = element(by.id('search_query_top'));
     this.searchButton = element(by.css('button[class*="button-search"]'));
     this.alertWarning = element(by.css('p[class="alert alert-warning"]'));
-    this.productContainers = element.all(by.css('div[class*="product-container"]'));
-    this.productNames = element.all(by.css('a[class*="product-name"]'));
-
-    this.logNames = function () {
-        this.productNames.each(function (productName) {
-            console.log(productName.getText())
-        })
-    }
+    this.bestSellersButton = element(by.css('a[class="blockbestsellers"]'));
+    /* 
+        this.productContainers = element.all(by.css('div[class*="product-container"]'));
+        this.productNames = element.all(by.css('a[class*="product-name"]')); */
 
 }
 
--- tests/home/home.spec.js
+++ tests/home/home.spec.js
@@ -6,14 +6,14 @@ describe('My store - home page', function () {
           browser.waitForAngularEnabled(false);
       }) */
 
-    it('Clicking search button opens new page with alert to enter a search keyword', function () {
+    it('Clicking search button opens new page with alert to enter a search keyword', async function () {
         browser.get(browser.baseUrl);
         home.searchButton.click();
         expect(home.alertWarning.getText()).toContain('enter a search keyword');
     });
 
-    fit('add to card', function () {
-       home.logNames();
+    fit('Click best sellers button displays best sellers', async function () {
+        home.bestSellersButton.click();
     });
 
 })
