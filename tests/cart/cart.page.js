4b08fbb7d002f6fd1bc437b5b1df3e1a4e09a402
Nemanja Jeremic <jnemanja@hotmail.com>
2019-03-12 09:52:46 +0100

    Revert to 4d7ca19bbbfbff26b098c41a51d5ccabc496e3b7

--- tests/cart/cart.page.js
+++ tests/cart/cart.page.js
@@ -10,12 +10,6 @@ let shoppingCart = function () {
     this.viewShoppingCart = element(by.css('a[title="View my shopping cart"]'));
     this.productRows = element.all(by.css('tr[class*="cart_item"]'));
     this.cartProceedToCheckoutButton1 = element(by.css('a[class*="button btn btn-default standard-checkout button-medium"]'));
-    this.cartProceedToCheckoutAddressButton = element(by.css('button[name="processAddress"]'));
-    this.termsOfServiceCheckbox = element(by.id('cgv'));
-    this.cartProceedToCheckoutShippingButton = element(by.css('button[name="processCarrier"]'));
-    this.payByBankWire = element(by.className('bankwire'));
-    this.confirmOrderButton = element(by.css('p[id="cart_navigation"] button[type="submit"]'));
-    this.orderConfirmationMessage = element(by.css('div[class="box"] p[class="cheque-indent"]'));
     this.quickViewAddToCartButton = element(by.css('p[id="add_to_cart"] button'));
     this.itemQuantity = element.all(by.css('input[class="cart_quantity_input form-control grey"]'));
     this.cartTrashIcons = element.all(by.css('a[class="cart_quantity_delete"]'));
@@ -28,19 +22,16 @@ let shoppingCart = function () {
                 currentItem = item;
                 browser.actions().mouseMove(currentItem).perform();
                 let addToCartButton = await currentItem.element(by.css('a[class="button ajax_add_to_cart_button btn btn-default"]'));
-                await addToCartButton.click();
+                addToCartButton.click();
 
             }
         })
-        //TODO - this is the current workaround if callback is not provided 
-        if (callback) {
-            callback(this.cartModal, 30000);
-            this.cartModalContinueShoppingButton.click();
-        }
-        browser.sleep(2000);
+
+        callback(this.cartModal, 30000);
+        this.cartModalContinueShoppingButton.click();
 
     }
-
+    // TODO - fix
     this.addMultipleProducts = function (productName, amount, callback) {
         for (let index = 0; index < amount; index++) {
             this.addToCart(productName, callback);
@@ -57,11 +48,10 @@ let shoppingCart = function () {
                 deleteButton.click();
             }
         });
-        browser.sleep(2000);
     }
 
-    this.clickOnItem = async function (productName) {
-        await this.productRows.each(async function (productRow) {
+    this.clickOnItem = function (productName) {
+        this.productRows.each(async function (productRow) {
             let cartItem = await productRow.element(by.css('p[class="product-name"] a'));
             if (productName === await cartItem.getText()) {
                 cartItem.click();
@@ -69,15 +59,14 @@ let shoppingCart = function () {
         });
     }
 
-    this.increaseItemQuantity = async function (productName) {
-        await this.productRows.each(async function (productRow) {
+    this.increaseItemQuantity = function (productName) {
+        this.productRows.each(async function (productRow) {
             let cartItemName = await productRow.element(by.css('p[class="product-name"] a')).getText();
             let increaseQuantityButton = await productRow.element(by.css('a[class="cart_quantity_up btn btn-default button-plus"]'));
             if (productName === cartItemName) {
                 increaseQuantityButton.click();
             }
         });
-        browser.sleep(3000);
     }
 
     this.decreaseItemQuantity = function (productName) {
@@ -88,21 +77,18 @@ let shoppingCart = function () {
                 decreaseQuantityButton.click();
             }
         });
-        browser.sleep(3000);
     }
 
-    this.checkItemQuantity = async function (productName /* , noOfProducts */ ) {
+    this.checkItemQuantity = function (productName, noOfProducts) {
         let currentRow;
-        let quantityValue;
-        await this.productRows.each(async function (productRow) {
+        this.productRows.each(async function (productRow) {
             let cartItemName = await productRow.element(by.css('p[class="product-name"] a')).getText();
             if (productName === cartItemName) {
                 currentRow = productRow;
             }
             quantityValue = await currentRow.element(by.css('input[class="cart_quantity_input form-control grey"]')).getAttribute('value');
+            return quantityValue;
         });
-
-        return await quantityValue;
     }
 
     this.removeAllItems = function () {
@@ -110,13 +96,17 @@ let shoppingCart = function () {
         /*  global.waitUntilVisible(cart.cartTrashIcons.first(), 30000) */
         this.cartTrashIcons.each(async function (trashIcon) {
             trashIcon.click();
-        });
-        browser.sleep(2000);
+        })
     }
-    //TODO - fix to work with product name - NOT index
+
     this.openQuickView = function (productIndex) {
+        //keep track of products added and convert the value to string
+        /*  this.productNumber += 1;
+         this.productsInCart = this.productNumber.toString(); */
+        //hover over product
         let currentProduct = this.productContainers.get(productIndex);
         browser.actions().mouseMove(currentProduct).perform();
+        //find "add to cart" button and click on it
         currentProduct.element(by.css('a[class="quick-view"]')).click();
     }
 
@@ -132,16 +122,6 @@ let shoppingCart = function () {
         });
 
         return x;
-    }
-
-    this.addViaQuickView = function (productIndex) {
-        this.openQuickView(productIndex);
-        browser.sleep(1000);
-        browser.switchTo().frame(element(by.css('iframe[class="fancybox-iframe"]')).getWebElement());
-        this.quickViewAddToCartButton.click();
-        browser.switchTo().defaultContent();
-        browser.sleep(3000);
-        this.cartModalContinueShoppingButton.click();
     }
 
 }
